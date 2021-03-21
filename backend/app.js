0c74134110383113e169f0bb86d23f30fa2bca09
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2021-03-22 00:10:04 +0800

    55. Fetching Data From a Database

--- backend/app.js
+++ backend/app.js
@@ -34,27 +34,13 @@ app.post('/api/post', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: '123',
-      title: 'fist title',
-      content: 'first content'
-    },
-    {
-      id: '1dwd3',
-      title: 'second title',
-      content: 'second content'
-    },
-    {
-      id: '1grg',
-      title: 'third title',
-      content: 'third content'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
-  });
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: documents
+      });
+    });
 });
 
 module.exports = app;
