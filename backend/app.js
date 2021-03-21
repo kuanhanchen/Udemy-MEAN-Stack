8d2f50f92df362514f0f29b0cfdb2d29b21cf269
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2021-03-21 21:38:11 +0800

    41. Adding the POST Backend Point (install body-parser)

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
+app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-with, Content-Type, Accept');
@@ -9,7 +10,14 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.post('./api/posts', (req, res, next) => {
+  const posts = req.body;
+  res.status(201).json({
+    message: 'post added successfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: '123',
