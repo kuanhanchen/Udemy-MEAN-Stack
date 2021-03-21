2b884c45a647efffea243cca108cb51cc0bc2dc8
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2021-03-21 14:26:38 +0800

    28. Calling GET Post (OnInit, rxjs, observables, Subject)

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Post } from '../post.model';
 import { PostsService } from '../posts.service';
 @Component({
@@ -6,13 +7,25 @@ import { PostsService } from '../posts.service';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.scss']
 })
-export class PostListComponent {
+export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
   //   { title: "First Post", content: "First Content" },
   //   { title: "Second Post", content: "Second Content" },
   //   { title: "Third Post", content: "Third Content" }
   // ];
-  @Input() posts: Post[] = [];
-
+  posts: Post[] = [];
+  private postsSub: Subscription;
   constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsService.getPostUpdatedListener()
+      .subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
