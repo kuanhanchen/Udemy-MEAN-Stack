07012b3f7c5dcbda0f80e2c4b7547846ac6514ec
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2021-03-21 15:55:00 +0800

    36. Adding the Express Framework

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const app = require('./backend/app');
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.end('kk');
-});
+app.set('port', port);
+const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.listen(port);
