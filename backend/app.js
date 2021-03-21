3bbb9f597ecf7bc543517fa1a76919687ec0f3d3
Kuan-Han Chen <kuanhanchen0608@gmail.com>
2021-03-21 16:31:33 +0800

    38. Fetching Initial Posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('response');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: '123',
+      title: 'fist title',
+      content: 'first content'
+    },
+    {
+      id: '1dwd3',
+      title: 'second title',
+      content: 'second content'
+    },
+    {
+      id: '1grg',
+      title: 'third title',
+      content: 'third content'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
